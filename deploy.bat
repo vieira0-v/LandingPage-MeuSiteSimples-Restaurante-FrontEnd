9d4b45c3a7f257155221887c8e150f48693ac33d
unknown <gabriellevieira199701@outlook.com>
2025-04-10 15:51:44 -0300

    Atualize para site demo netlify e github

--- deploy.bat
+++ deploy.bat
@@ -1,14 +1,22 @@
 @echo off
-echo ===============================
-echo  💬 Digite a mensagem do commit:
-echo ===============================
+title 🚀 Deploy automático - por Shy 💻
+color 0a
+
+echo ================================
+echo 👩‍🍳 Olá, Shy! Vamos publicar seu site? 
+echo ================================
+echo 💬 Digite a mensagem do commit:
 set /p mensagem=
 
 git add .
 git commit -m "%mensagem%"
 git push
 
-echo ===============================
-echo 🚀 Código enviado com sucesso!
-echo ===============================
+echo.
+echo ================================
+echo ✅ Código enviado com sucesso!
+echo 🌐 Acesse seu site em:
+echo 👉 https://seusite.netlify.app
+echo ================================
+
 pause
